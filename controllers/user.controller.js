f0c70284af61f309aab97df5fcca202cd5653da1
Pranay Kumar <pranaykumar31may1996@gmail.com>
2021-04-10 19:05:02 +0530

    Hiding the password and storing the data

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -5,7 +5,7 @@ const router = express.Router();
 const User = require("../models/user.model");
 
 router.get("/", async(req,res) => {
-    const users = await User.find({}).lean().exec()
+    const users = await User.find({}).select("-password").lean().exec()
     return res.status(200).json({data: users})
 })
 
