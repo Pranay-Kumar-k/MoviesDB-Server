4d58c7bc114b904f9d73eb1354ede51b25d124dd
Pranay Kumar <pranaykumar31may1996@gmail.com>
2021-04-11 00:50:56 +0530

    Get Movie by ID request successfull

--- controllers/movie.controller.js
+++ controllers/movie.controller.js
@@ -5,7 +5,7 @@ const router = express.Router();
 const Movie = require("../models/movie.model");
 
 // Route to get all the movies
-const getMovies = async(req,res) => {
+const getMovies = async (req,res) => {
     try {
         const movies = await Movie.find({}).lean().exec()
         return res.status(200).json({data:movies})
@@ -16,7 +16,7 @@ const getMovies = async(req,res) => {
 }
 
 // Create a new movie item and post it to database
-const postMovie = async(req,res) => {
+const postMovie = async (req,res) => {
     const {id,movie_title,genre,released_year} = req.body;
     try{
         const newMovie = new Movie({id,movie_title,genre,released_year})
@@ -29,6 +29,22 @@ const postMovie = async(req,res) => {
     }
 }
 
+// Get a movie by its ID 
+const getMovieById = async (req,res) => {
+    const {id} = req.params;
+    console.log(id)
+    try {
+        const movie = await Movie.findOne({id}).lean().exec()
+        console.log(movie,id)
+        return res.status(200).json({data:movie})
+    }
+    catch(err) {
+        return res.status(404).json({status:"failed", message:"Item not found"})
+    }
+}
+
+
 router.get("/", getMovies);
 router.post("/", postMovie);
+router.get("/:id", getMovieById);
 module.exports = router;
