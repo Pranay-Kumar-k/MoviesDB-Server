895f900017febf07438e3fd3f102c5ce0d6947b5
Pranay Kumar <pranaykumar31may1996@gmail.com>
2021-04-11 00:39:07 +0530

    Post new movie request successfull

--- controllers/movie.controller.js
+++ controllers/movie.controller.js
@@ -6,9 +6,29 @@ const Movie = require("../models/movie.model");
 
 // Route to get all the movies
 const getMovies = async(req,res) => {
-    const movies = await Movie.find({}).lean().exec()
-    return res.status(200).json({data:movies})
+    try {
+        const movies = await Movie.find({}).lean().exec()
+        return res.status(200).json({data:movies})
+    }
+    catch (err) {
+        return res.status(404).json({status:'failed', message:"Not Found"})
+    }
+}
+
+// Create a new movie item and post it to database
+const postMovie = async(req,res) => {
+    const {id,movie_title,genre,released_year} = req.body;
+    try{
+        const newMovie = new Movie({id,movie_title,genre,released_year})
+        await newMovie.save();
+        // console.log(newMovie);
+        return res.status(201).json({status:"success", message:"New movie added successfully"});
+    }
+    catch(err) {
+        return res.status(400).json({status:"failed", message:"Post movie not successful"})
+    }
 }
 
 router.get("/", getMovies);
+router.post("/", postMovie);
 module.exports = router;
--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -5,14 +5,14 @@ const router = express.Router();
 const User = require("../models/user.model");
 
 const getAllUsers = async (req,res) => {
-    const users = await User.find({}).lean().exec()
-    console.log(users)
-    return res.status(200).json({data: users})
-}
-
-const postMovie = async(req,res) => {
-    const {id,movie_title,genre,released_year} = req.body;
-    
+    try{
+        const users = await User.find({}).lean().exec()
+        console.log(users)
+        return res.status(200).json({data: users})
+    }
+    catch(err) {
+        res.status(404).json({status:"failed", message:"Not Found"})
+    }
 }
 
 router.get("/", getAllUsers);
